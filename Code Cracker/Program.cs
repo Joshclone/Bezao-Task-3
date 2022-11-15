27ce45d018ca255910bf19986acf614ed9591946
Nwaeze Joshua <nwaezejoshua@gmail.com>
2022-11-15 10:45:31 +0100

    Update Program.cs

--- Code Cracker/Program.cs
+++ Code Cracker/Program.cs
@@ -1,57 +1,82 @@
-﻿using System;
-
-class SubstitutionCipher
+using System;
+using System.Xml;
+using System.Text;
+using System.Security.Cryptography;
+using System.Xml.Serialization;
+namespace ConsoleApp
 {
-    static void Main()
+
+
+    public class RsaEncryption
     {
-        string key = "jfkgotmyvhspcandxlrwebquiz";
-        string plainText = "hi joshclone welcome to bezao task encryption and decryption program";
-        string cipherText = Encrypt(plainText, key);
-        string decryptedText = Decrypt(cipherText, key);
+        private static RSACryptoServiceProvider csp = new RSACryptoServiceProvider(2048);
+        private RSAParameters _privateKey;
+        private RSAParameters _publicKey;
 
-        Console.WriteLine("Plain     : {0}", plainText);
-        Console.WriteLine("Encrypted : {0}", cipherText);
-        Console.WriteLine("Decrypted : {0}", plainText);
-        Console.ReadKey();
-    }
-
-    static string Encrypt(string plainText, string key)
-    {
-        char[] chars = new char[plainText.Length];
-
-        for (int i = 0; i < plainText.Length; i++)
+        public RsaEncryption()
         {
-            if (plainText[i] == ' ')
-            {
-                chars[i] = ' ';
-            }
-            else
-            {
-                int j = plainText[i] - 97;
-                chars[i] = key[j];
-            }
+
+            _privateKey = csp.ExportParameters(true);
+            _publicKey = csp.ExportParameters(false);
+
         }
 
-        return new string(chars);
-    }
-
-    static string Decrypt(string cipherText, string key)
-    {
-        char[] chars = new char[cipherText.Length];
-
-        for (int i = 0; i < cipherText.Length; i++)
+        public string GetPublicKey()
         {
-            if (cipherText[i] == ' ')
-            {
-                chars[i] = ' ';
-            }
-            else
-            {
-                int j = key.IndexOf(cipherText[i]) - 97;
-                chars[i] = (char)j;
-            }
+            var sw = new StringWriter();
+            var xs = new XmlSerializer(typeof(RSAParameters));
+            xs.Serialize(sw, _publicKey);
+            return sw.ToString();
+
         }
 
-        return new string(chars);
+
+        public string Encrypt(string plainText)
+        {
+            csp = new RSACryptoServiceProvider();
+            csp.ImportParameters(_publicKey);
+            var data = Encoding.Unicode.GetBytes(plainText);
+            var cypher = csp.Encrypt(data, false);
+            return Convert.ToBase64String(cypher);
+        }
+
+
+        //Decrypt message
+        public string Decrypt(string cypherText)
+        {
+            var dataBytes = Convert.FromBase64String(cypherText);
+            csp.ImportParameters(_privateKey);
+            var plainText = csp.Decrypt(dataBytes, false);
+            return Encoding.Unicode.GetString(plainText);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            RsaEncryption rsa = new RsaEncryption();
+            string cypher = string.Empty;
+
+
+            Console.WriteLine($"Public Key: {rsa.GetPublicKey()} \n");
+
+
+
+            Console.WriteLine("enter your text to encrypt");
+            var text = Console.ReadLine();
+            if (!string.IsNullOrEmpty(text))
+            {
+                cypher = rsa.Encrypt(text);
+                Console.WriteLine($"Encrypted Text: {cypher}");
+            }
+            Console.WriteLine("press any key to decrypt text");
+            Console.ReadLine();
+
+            var plainText = rsa.Decrypt(cypher);
+        }
     }
 }
